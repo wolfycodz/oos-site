a9881b86062792e6663e2497e4db420e4f23972e
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-03 11:32:22 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Orbit OS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Orbit OS:" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          we test the bounderys of software, hardware and security.
         </p>
       </main>
 
