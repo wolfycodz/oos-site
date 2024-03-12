c05c6c2a5f26e7cfd7aa3dfaec1496986706d511
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-11 21:27:07 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           we test the bounderys of software, hardware and security. we're also open source!
         </p>
+    <iframe src="https://discordapp.com/widget?id=1216948283784691792&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </main>
 
       <Footer />
